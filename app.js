18e2c8807d2eaa4a4caccd8d103eaf61e3dc3326
Sivakumaran E <sivakumaran.e@gmail.com>
2020-05-05 20:06:35 +0530

    complete game

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 // UI elements
@@ -16,7 +16,18 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener('mousedown', function (e) {
+  if (e.target.className === 'play-again') {
+    //console.log('clicked via delegation')
+    window.location.reload();
+  }
+});
+
+
+// Listen for guess
 guessBtn.addEventListener('click', function () {
+  console.log('guess button clicked')
   let guess = parseInt(guessInput.value);
 
   if (isNaN(guess) || guess < min || guess > max) {
@@ -25,12 +36,47 @@ guessBtn.addEventListener('click', function () {
 
   // Check winning num
   if (guess === winningNum) {
-    guessInput.disabled = true;
-    guessInput.style.borderColor = 'green';
-    setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+
+    // Game over, win
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+
+  } else {
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+
+      // Game over, lost
+      gameOver(false, `Game over, You lost. The correct number was ${winningNum}`);
+
+    } else {
+
+      //Game continues - answer wrong
+      guessInput.style.borderColor = 'red';
+      guessesLeft.value = '';
+      setMessage(`${guess} is not correct. ${guessesLeft} guesses left`, 'red');
+    }
   }
 
-})
+});
+
+// Game over 
+function gameOver(won, msg) {
+  guessInput.disabled = true;
+
+  // set color to red or green
+  const color = (won === true) ? 'green' : 'red';
+
+  guessInput.style.borderColor = color
+  setMessage(msg, color);
+
+  // Play again
+  guessBtn.value = 'Play Again';
+  guessBtn.className = 'play-again';
+}
+
+function getRandomNum(min, max) {
+  return Math.floor((Math.random() * (max - min + 1) + min));
+}
 
 function setMessage(msg, color) {
   message.style.color = color;
